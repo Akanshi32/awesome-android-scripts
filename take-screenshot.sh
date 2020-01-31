7b72da147c68c4840f4f77bd1e52296a260a0d6a
Akanshi Srivastava <akanshi32@gmail.com>
2020-01-31 15:31:35 +0530

    Update take-screenshot.sh

--- take-screenshot.sh
+++ take-screenshot.sh
@@ -3,5 +3,5 @@
 # Take an screenshot into "screenshots/" using current date/time as file name.
 # src: https://gist.github.com/FokkeZB/86713457bf84808a12a6
 #
-
+echo "Taking screenshot"
 adb shell screencap -p | perl -pe "s/\x0D\x0A/\x0A/g" > "screenshots/$(date +%Y%m%d-%H%M%S).png"
